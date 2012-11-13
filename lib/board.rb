8c1bc157d7b1d63b277799434a5d14eb88aaec00
Sam Phippen <samphippen@googlemail.com>
2012-11-13 00:18:33 +0000

    Add code for computing concreteness and specs
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- lib/board.rb
+++ lib/board.rb
@@ -3,6 +3,16 @@ class Board
     @board = make_board
   end
 
+  def to_s
+    @board.map do |row|
+      p1 = row.map {|cell| {nil => ".",
+                            :player => "P",
+                            :opponent => "o"}[cell[:color]]}.join(" ")
+      p2 = row.map {|cell| cell[:letter]}.join(" ")
+      "#{p1} #{p2}"
+    end.join("\n")
+  end
+
   def score_board(player)
     @board.flatten.select {|square| square[:color] == player}.count
   end
@@ -13,7 +23,48 @@ class Board
     end
   end
 
+  def update_concreteness
+    5.times do |y|
+      5.times do |x|
+        c = neighbor_count x,y
+        required = 4
+        required -= 1 if isedgey y
+        required -= 1 if isedgex x
+        if c >= required and @board[y][x][:color] != nil
+          concrete x,y
+        end
+      end
+    end
+  end
+
+  def cell(x,y)
+    @board[y][x]
+  end
+
   private
+
+  def neighbor_count(x, y)
+    current_color = @board[y][x][:color]
+    r = 0
+    r += 1 if @board[y-1][x][:color] == current_color unless y == 0
+    r += 1 if @board[y+1][x][:color] == current_color unless y == 4
+    r += 1 if @board[y][x+1][:color] == current_color unless x == 4
+    r += 1 if @board[y][x-1][:color] == current_color unless x == 0
+    return r
+  end
+
+  def concrete(x,y)
+    @board[y][x][:concrete] = true
+  end
+
+  def isedgey(y)
+    return y == 0 || y == 4
+  end
+
+  def isedgex(x)
+    return x == 0 || x == 4
+  end
+
   def make_board
     board = []
     5.times do
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -23,4 +23,98 @@ describe Board do
       subject.score_board(:opponent).should == 0
     end
   end
+
+  describe "#update_concreteness" do
+    it "doesn't concrete anything when the board is fresh" do
+      subject.update_concreteness
+      subject.should_not_receive :concrete
+    end
+
+    it "concretes the top left corner when the three tiles are taken" do
+      subject.capture_square(0,0,:player)
+      subject.capture_square(0,1,:player)
+      subject.capture_square(1,0,:player)
+      subject.update_concreteness
+      subject.cell(0,0)[:concrete].should be_true
+    end
+
+    it "concretes the top right corner when the three tiles are taken" do
+      subject.capture_square(4,0,:player)
+      subject.capture_square(3,0,:player)
+      subject.capture_square(4,1,:player)
+      subject.update_concreteness
+      subject.cell(4,0)[:concrete].should be_true
+    end
+
+    it "concretes the bottom left corner when the three tiles are taken" do
+      subject.capture_square(0,4,:player)
+      subject.capture_square(1,4,:player)
+      subject.capture_square(0,3,:player)
+      subject.update_concreteness
+      subject.cell(0,4)[:concrete].should be_true
+    end
+
+    it "concretes the bottom right corner when the three tiles are taken" do
+      subject.capture_square(4,4,:player)
+      subject.capture_square(3,4,:player)
+      subject.capture_square(4,3,:player)
+      subject.update_concreteness
+      subject.cell(4,4)[:concrete].should be_true
+    end
+
+    it "doesn't concrete a cell which is surrounded but not captured" do
+      subject.capture_square(2,2,:player)
+      subject.capture_square(3,3,:player)
+      subject.capture_square(1,3,:player)
+      subject.capture_square(2,4,:player)
+      subject.update_concreteness
+      subject.cell(2,3)[:concrete].should be_false
+    end
+
+    it "concretes a cell on the top edge with the three surrounding cells captured" do
+      subject.capture_square(2,0,:player)
+      subject.capture_square(1,0,:player)
+      subject.capture_square(3,0,:player)
+      subject.capture_square(2,1,:player)
+      subject.update_concreteness
+      subject.cell(2,0)[:concrete].should be_true
+    end
+
+    it "concretes a cell on the bottom edge with the three surrounding cells captured" do
+      subject.capture_square(2,4,:player)
+      subject.capture_square(1,4,:player)
+      subject.capture_square(3,4,:player)
+      subject.capture_square(2,3,:player)
+      subject.update_concreteness
+      subject.cell(2,4)[:concrete].should be_true
+    end
+
+    it "concretes a cell on the left edge with the three surrounding cells captured" do
+      subject.capture_square(0,2,:player)
+      subject.capture_square(0,1,:player)
+      subject.capture_square(0,3,:player)
+      subject.capture_square(1,2,:player)
+      subject.update_concreteness
+      subject.cell(0,2)[:concrete].should be_true
+    end
+
+    it "concretes a cell on the right edge with the three surrounding cells captured" do
+      subject.capture_square(4,2,:player)
+      subject.capture_square(4,1,:player)
+      subject.capture_square(4,3,:player)
+      subject.capture_square(3,2,:player)
+      subject.update_concreteness
+      subject.cell(4,2)[:concrete].should be_true
+    end
+
+    it "concretes a cell when it's captured and all 4 cells surround it" do
+      subject.capture_square(2,2,:player)
+      subject.capture_square(2,3,:player)
+      subject.capture_square(3,2,:player)
+      subject.capture_square(1,2,:player)
+      subject.capture_square(2,1,:player)
+      subject.update_concreteness
+      subject.cell(2,2)[:concrete].should be_true
+    end
+  end
 end
